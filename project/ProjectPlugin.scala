aa2912c87e93b9c0d94930048924958c989057a3
Juan Pedro Moreno <juanpmorest@gmail.com>
2020-04-02 19:25:03 +0200

    Adds sbt-github and correct settings

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -2,9 +2,13 @@ import sbt.Keys._
 import sbt.ScriptedPlugin.autoImport._
 import sbt._
 
+import com.alejandrohdezma.sbt.github.SbtGithubPlugin
+
 import scala.language.reflectiveCalls
 
 object ProjectPlugin extends AutoPlugin {
+
+  override def requires = SbtGithubPlugin
 
   override def trigger: PluginTrigger = allRequirements
 
@@ -24,9 +28,9 @@ object ProjectPlugin extends AutoPlugin {
     }
 
     lazy val noPublishSettings = Seq(
-      publish           := ((): Unit),
-      publishLocal      := ((): Unit),
-      publishArtifact   := false,
+      publish := ((): Unit),
+      publishLocal := ((): Unit),
+      publishArtifact := false,
       publishMavenStyle := false // suppress warnings about intransitive deps (not published anyway)
     )
 
@@ -63,12 +67,12 @@ object ProjectPlugin extends AutoPlugin {
     Seq(
       description := "mu RPC is a purely functional library for " +
         "building RPC endpoint based services with support for RPC and HTTP/2",
-      name                 := "sbt-mu-srcgen",
-      startYear            := Some(2017),
-      organization         := "io.higherkindness",
-      organizationName     := "47 Degrees",
+      name := "sbt-mu-srcgen",
+      startYear := Some(2017),
+      organization := "io.higherkindness",
+      organizationName := "47 Degrees",
       organizationHomepage := Some(url("http://47deg.com")),
-      scalaVersion         := V.scala,
-      crossScalaVersions   := Seq(V.scala)
+      scalaVersion := V.scala,
+      crossScalaVersions := Seq(V.scala)
     )
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -6,8 +6,9 @@ addSbtPlugin("org.scalameta"             % "sbt-scalafmt"      % "2.3.2")
 addSbtPlugin("org.scalameta"             % "sbt-mdoc"          % "2.1.3")
 addSbtPlugin("com.geirsson"              % "sbt-ci-release"    % "1.5.2")
 addSbtPlugin("de.heikoseeberger"         % "sbt-header"        % "5.4.0")
-addSbtPlugin("com.alejandrohdezma"       % "sbt-github-mdoc"   % "0.6.0")
+addSbtPlugin("com.alejandrohdezma"       %% "sbt-github"       % "0.6.0")
 addSbtPlugin("com.alejandrohdezma"       % "sbt-github-header" % "0.6.0")
+addSbtPlugin("com.alejandrohdezma"       % "sbt-github-mdoc"   % "0.6.0")
 addSbtPlugin("com.alejandrohdezma"       % "sbt-mdoc-toc"      % "0.2")
 
 libraryDependencies += "org.scala-sbt" %% "scripted-plugin" % sbtVersion.value
