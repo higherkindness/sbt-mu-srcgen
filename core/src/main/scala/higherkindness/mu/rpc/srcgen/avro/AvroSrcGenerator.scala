e06116cf83475a2f653c5cc41d2eedc09f830058
Dylan Martin <dylan.martin@47deg.com>
2020-07-28 15:57:36 -0700

    got it

--- core/src/main/scala/higherkindness/mu/rpc/srcgen/avro/AvroSrcGenerator.scala
+++ core/src/main/scala/higherkindness/mu/rpc/srcgen/avro/AvroSrcGenerator.scala
@@ -167,7 +167,6 @@ final case class AvroSrcGenerator(
 
   private def parseMessage(name: String, request: Schema, response: Schema): String = {
     val requestArgs = request.getFields.asScala
-    println(s"the request ${request.getName} looks like this ${requestArgs.toString}")
     if (requestArgs.size > 1)
       throw ParseException("RPC method has more than 1 request parameter")
     val requestParam = {
@@ -180,14 +179,11 @@ final case class AvroSrcGenerator(
         s"${requestArg.name}: ${requestArg.schema.getFullName}"
       }
     }
-    val responseArgs = response.getFields.asScala
-    println(s"the response ${response.getName} looks like this ${responseArgs.toString}")
     val responseParam = {
-      val responseArg = responseArgs.head
-      if (responseArg.schema.getType == Schema.Type.NULL) EmptyType
+      if (response.getType == Schema.Type.NULL) EmptyType
       else {
-        if (responseArg.schema.getType != Schema.Type.RECORD)
-          throw ParseException("RPC method is not a record type")
+        if (response.getType != Schema.Type.RECORD)
+          throw ParseException("RPC method response parameter is not a record type")        
         s"${response.getNamespace}.${response.getName}"
       }
     }
